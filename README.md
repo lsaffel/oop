8aa7f40ad4d80ab44227f94cf64f0bbe566d71fc
Lisa <llvvss@icloud.com>
2024-03-29 19:25:51 -0600

    adding constructor and second object

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ OOP
 Class and object  
 Assigns values to the properties of the object  
 Then prints them
+
+Then, add a constructor to the class and pass the attributes that way
--- src/oop.cpp
+++ src/oop.cpp
@@ -8,17 +8,27 @@ class YouTubeChannel {
     string OwnerName;
     int SubscribersCount;
     list<string> PublishedVideoTitles;  // #include <list> is needed for this
+
+    // create a constructor for this class
+    YouTubeChannel(string name, string ownerName) {
+        Name = name;
+        OwnerName = ownerName;
+        SubscribersCount = 0;   // when we create a channel, it will 
+            // initially have 0 subscribers every time
+    }
 };
 
 int main() {
     // create an object
-    YouTubeChannel ytChannel;
 
     // assign values to the attributes(?) of the object
-    ytChannel.Name = "CodeBeauty";
-    ytChannel.OwnerName = "Saldina";
-    ytChannel.SubscribersCount = 1800;
-    ytChannel.PublishedVideoTitles = {"C++ for beginners video 1", "HTML & CSS video 1", "C++ OOP Video 1"};
+    // ytChannel.Name = "CodeBeauty";
+    // ytChannel.OwnerName = "Saldina";
+    // ytChannel.SubscribersCount = 1800;
+    // ytChannel.PublishedVideoTitles = {"C++ for beginners video 1", "HTML & CSS video 1", "C++ OOP Video 1"};
+
+    // new way of doing the above code, using a constructor instead:
+    YouTubeChannel ytChannel("CodeBeauty", "Saldina");
 
     cout << "Name: " << ytChannel.Name << endl;
     cout << "OwnerName: " << ytChannel.OwnerName << endl;
@@ -27,6 +37,18 @@ int main() {
     cout << "Videos: " << endl;
 
     for(string videoTitle: ytChannel.PublishedVideoTitles){
+        cout << videoTitle << endl;
+    } 
+
+    YouTubeChannel ytChannel2("AmySings", "Amy");
+
+    cout << "Name: " << ytChannel2.Name << endl;
+    cout << "OwnerName: " << ytChannel2.OwnerName << endl;
+    cout << "SubscribersCount: " << ytChannel2.SubscribersCount << endl;
+
+    cout << "Videos: " << endl;
+
+    for(string videoTitle: ytChannel2.PublishedVideoTitles){
         cout << videoTitle << endl;
     } 
 }
