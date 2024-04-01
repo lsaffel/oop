8bc35ea73d1be94e1c5dcb2d31ed2264c62ca482
Lisa <llvvss@icloud.com>
2024-04-01 12:10:28 -0600

    new build

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ Then, add a class method to print the info, getInfo()
 Then, added subscribe and unsubscribe methods  
 Encapsulate the PublishVideo method  
 Add an inherited class, CookingYouTubeChannel
+
+private properties are available only in that class  
+protected properties are available in derived classes also
--- src/oop.cpp
+++ src/oop.cpp
@@ -8,10 +8,11 @@ private:
     int SubscribersCount;
     list<string> PublishedVideoTitles;  // #include <list> is needed for this
 
-    // protected is needed here to allow the OwnerName 
-    // to be accessed in the derived class
+    // protected is needed here to allow these properties
+    // to be accessed in the derived classes
 protected:
     string OwnerName;
+    int ContentQuality;
 
 public:
     // create a constructor for this class
@@ -20,6 +21,7 @@ public:
         OwnerName = ownerName;
         SubscribersCount = 0;   // when we create a channel, it will 
             // initially have 0 subscribers every time
+        ContentQuality = 0; // initialize 
     }
 
     void GetInfo() {
@@ -48,8 +50,16 @@ public:
     // adds a new title to the videos list
     void PublishVideo(string title) {
         PublishedVideoTitles.push_back(title);
+    }
+
+    void CheckAnalytics() {
+        if (ContentQuality < 5)
+            cout << Name << " has low quality content." << endl;
+        else
+            cout << Name << " has great content." << endl;
 
     }
+
 };
 
 class CookingYouTubeChannel: public YouTubeChannel {
@@ -63,22 +73,37 @@ public:
     }
     void Practice() {
         cout<< OwnerName << " is practising cooking, learning new recipes, experimenting with spices..." << endl;
+        ContentQuality++;
     }
 };
 
+class SingersYouTubeChannel: public YouTubeChannel {
+    // add a constructor:
+public:
+    SingersYouTubeChannel(string name, string ownerName): YouTubeChannel(name, ownerName) {
+        // the name and ownerName don't have to be initialized 
+        // because it's inherited and this class already
+        // knows how to initialize them. That's the purpose of
+        // : YouTubeChannel(name, ownerName)
+    }
+    void Practice() {
+        cout<< OwnerName << " is taking singing classes, learning new songs, learning how to dance..." << endl;
+        ContentQuality++;
+
+    }
+};
+
+
 int main() {
     CookingYouTubeChannel cookingYtChannel("Amy's Kitchen", "Amy");
-    CookingYouTubeChannel cookingYtChannel2("John's Kitchen", "John");
-    cookingYtChannel.PublishVideo("Apple pie");
-    cookingYtChannel.PublishVideo("Chocolate cake");
-    cookingYtChannel.Subscribe();
-    cookingYtChannel.Subscribe();
-    cookingYtChannel.GetInfo();
-
-    cookingYtChannel.Practice();
-
-
-
+    SingersYouTubeChannel singersYtChannel("JohnSings", "John");
   
+    cookingYtChannel.Practice();
+    singersYtChannel.Practice();
+    singersYtChannel.Practice();
+    singersYtChannel.Practice();
+    singersYtChannel.Practice();
+    singersYtChannel.Practice();
+
     return 0;
 }
